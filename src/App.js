ac8cdd7a0dc7087b8da5ca21763c8f93ea4ab147
Sylek <sylek38@gmail.com>
2021-11-09 16:11:22 +0100

    add dark/light theme state toggle with icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './styles/App.sass';
 import Header from './components/Header';
 import Main from './components/Main';
+import { useState, useEffect } from 'react/cjs/react.development';
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+
+  useEffect(() => {
+    document.body.className = theme;
+}, [theme]);
+
   return (
     <div className="container">
       
-      <Header />
+      <Header theme={theme} setTheme={setTheme}/>
       <Main />
       
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,23 @@
 import './../styles/App.sass';
+import { ReactComponent as MoonIcon} from './../assets/icons/icon-moon.svg';
+import { ReactComponent as SunIcon} from './../assets/icons/icon-sun.svg';
 
-const Header = () => {
+const Header = ({ theme, setTheme }) => {
+
+    const themeHandler = () => {
+        setTheme(theme === "dark" ? "light" : "dark");
+    }
+
     return ( 
         <header className="header">
             <div className="header__container">
                 <h1 className="header__title">TODO</h1>
-                <button className="header__btn-switch-theme">Icon</button>
+                <button 
+                    onClick={themeHandler}
+                    className="header__btn-switch-theme"
+                >
+                    {theme === "dark" ? <SunIcon/> : <MoonIcon/>}
+                </button>
             </div>
         </header>
      );
